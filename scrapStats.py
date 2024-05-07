d6fcf68c43ae56bfb280cfee0985dc9f8ac3f568
m2kegg <mike.v05@bk.ru>
2024-05-07 22:57:40 +0300

    Добаввлена функция, получающая данные по конкретной дате

--- scrapStats.py
+++ scrapStats.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+from datetime import date, timedelta, datetime
 
 
-def getCurrentData():
+def get_сurrent_data():
     filter = ['Москва', 'Московская область', 'Белгородская область', 'Брянская область', 'Владимирская область',
               'Воронежская область', 'Ивановская область', 'Калужская область', 'Костромская область', 'Курская область'
-                                                                                                       'Липецкая область',
-              'Орловская область', 'Рязанская область', 'Смоленская область', 'Тамбовская область',
-              'Тверская область', 'Тульская область', 'Ярославская область']
+                                                                                                       'Орловская область',
+              'Рязанская область', 'Смоленская область', 'Тамбовская область',
+              'Тверская область', 'Тульская область', 'Ярославская область', 'Липецкая область', ]
 
     url_stat = "https://russian-trade.com/coronavirus-russia/"  # откуда берём данные
     response = requests.get(url_stat)
@@ -23,14 +24,55 @@ def getCurrentData():
             dict_covid = {}
             if data.find("a").string in filter:  # Проверка на нахождение региона в ЦФО:
                 data_all = data.find_all("td")
+                print(data_all)
                 dict_covid["Регион"] = data.find("a").string
-                dict_covid["Количество заболевших (абс)"] = int(data_all[2].contents[0])
-                dict_covid["Количество заболевших (отн)"] = int(data_all[2].contents[2].contents[0][1:-1])
-                dict_covid["Количество умерших (абс)"] = int(data_all[3].contents[0])
-                dict_covid["Количество умерших (отн)"] = int(data_all[3].contents[2].contents[0][1:-1])
-                dict_covid["Количество выздоровевших (абс)"] = int(data_all[4].contents[0])
-                dict_covid["Количество выздоровевших (отн)"] = int(data_all[4].contents[2].contents[0][1:-1])
-                dict_covid["Количество оставшихся заболевших (абс)"] = int(data_all[5].contents[0])
-                dict_covid["Количество оставшихся заболевших (отн)"] = int(data_all[5].contents[2].contents[0][1:-1])
+                dict_covid["Количество заболевших"] = int(data_all[2].contents[0])
+                #dict_covid["Количество заболевших (отн)"] = int(data_all[2].contents[2].contents[0][1:-1])
+                dict_covid["Количество умерших"] = int(data_all[3].contents[0])
+                #dict_covid["Количество умерших (отн)"] = int(data_all[3].contents[2].contents[0][1:-1])
+                dict_covid["Количество выздоровевших"] = int(data_all[4].contents[0])
+                #dict_covid["Количество выздоровевших (отн)"] = int(data_all[4].contents[2].contents[0][1:-1])
+                dict_covid["Количество оставшихся заболевших"] = int(data_all[5].contents[0])
+                #dict_covid["Количество оставшихся заболевших (отн)"] = int(data_all[5].contents[2].contents[0][1:-1])
                 result_data.append(dict_covid)
     return result_data
+
+
+def get_data_by_day(day_request):
+    filter = ['Москва', 'Московская область', 'Белгородская область', 'Брянская область', 'Владимирская область',
+              'Воронежская область', 'Ивановская область', 'Калужская область',
+              'Костромская область', 'Курская область''Орловская область',
+              'Рязанская область', 'Смоленская область', 'Тамбовская область',
+              'Тверская область', 'Тульская область', 'Ярославская область', 'Липецкая область', ]
+
+    url_main = "https://russian-trade.com/coronavirus-russia/"
+    url_reg = "https://russian-trade.com"
+
+    response = requests.get(url_main)
+    result_data = []
+    page = BeautifulSoup(response.text, 'html.parser')  # получили страницу для парсинга
+    table = page.find('table', class_='grid_exch')  # получили таблицу с данными
+    for data in table:
+        if data.find("th") is not None and data.find(
+                "th") != -1:  # Выбрасываем первую строку таблицы (с названиям полей)
+            continue
+        if data.find("a") != -1:  # Выбрасываем строки вида \n
+            dict_covid = {}
+            if data.find("a").string in filter:  # Проверка на нахождение региона в ЦФО:
+                href = url_reg + data.find("a")['href']  # Ссылка на подробную статистику
+                response_reg = requests.get(href)
+                reg_data = BeautifulSoup(response_reg.text, 'html.parser')
+                table_reg = reg_data.find('table')
+                for data_1 in table_reg:
+                    if len(data_1.find_all("td")) != 0:
+                        date_reg = data_1.find_all('td')[0].text
+                        date_reg_format = datetime.strptime(date_reg, '%d.%m.%Y')
+                        if date_reg_format == day_request:
+                            dict_covid["Регион"] = data.find("a").string
+                            dict_covid["Количество заболевших"] = int(data_1.find_all('td')[1].text)
+                            dict_covid["Количество умерших"] = int(data_1.find_all('td')[2].text)
+                            dict_covid["Количество выздоровевших"] = int(data_1.find_all('td')[3].text)
+                            dict_covid["Количество оставшихся заболевших"] = int(data_1.find_all('td')[4].text)
+                            result_data.append(dict_covid)
+    return result_data
+
