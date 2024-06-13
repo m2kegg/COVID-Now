9b604d2f1a8c0d92708e3895f0b885f3a5d9bde4
Везелев Михаил Вячеславович <mvvezelev@edu.hse.ru>
2024-06-13 23:05:54 +0300

    Починены графики под масштабирование экрана

--- form_diagrams.py
+++ form_diagrams.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from screeninfo import get_monitors
 
-def do_graph(df, axis, type_inf, color):
+def do_graph(df, axis, type_inf, color, font_size):
     """
     Функция, формирующая линейный график по датафрейму
 
@@ -20,32 +20,31 @@ def do_graph(df, axis, type_inf, color):
     "Количество заболевших", "Количество умерших", "Количество выздоровевших")
 
     Параметр color - цвет графика (строка, задаётся через HEX или названием цвета на англ.
+
+    Параметр font_size - размер шрифта для графика
     """
 
     if len(df) > 834:
         df = df.drop([834]) # фикс неприятного бага
     sns.lineplot(x="Дата", y=type_inf, data=df, ax=axis, color=color)
     axis.tick_params(axis='x', rotation=45)
+    axis.set_xlabel("Дата", fontsize=font_size)
+    axis.set_ylabel(type_inf, fontsize=font_size)
 
-    axis.set_xticklabels(axis.get_xticklabels(), fontsize=8)
-    axis.set_yticklabels(axis.get_yticklabels(), fontsize=8)
+
+    axis.set_xticklabels(axis.get_xticklabels(), fontsize=font_size)
+    axis.set_yticklabels(axis.get_yticklabels(), fontsize=font_size)
 
     axis.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".\
                                                      format(int(x)).replace(',', ' ')))
 
     match type_inf:
         case "Количество заболевших":
-            axis.set_title("График заболеваемости COVID-19")
+            axis.set_title("График заболеваемости COVID-19", fontsize=font_size)
         case "Количество умерших":
-            axis.set_title("График смертности от COVID-19")
+            axis.set_title("График смертности от COVID-19", fontsize=font_size)
         case "Количество выздоровевших":
-            axis.set_title("График выздоровлений от COVID-19")
-
-def get_screen_scale():
-    monitor = get_monitors()[0]
-    dpi = monitor.width / monitor.width_mm * 25.4  #
-    return dpi / 96
-
+            axis.set_title("График выздоровлений от COVID-19", fontsize=font_size)
 
 def make_figure(figsize=(5, 3.5)):
     """
@@ -57,11 +56,12 @@ def make_figure(figsize=(5, 3.5)):
     """
     fig, ax = plt.subplots(figsize=figsize)
 
-    scale = get_screen_scale()
-
-    plt.rc('figure', titlesize=14 * scale * 1.3)
-
     return fig, ax
+
+def get_font_size(width, height):
+    base_size = 8
+    scale = min(width, height) / 400
+    return int(base_size * scale)
 
 
 def create_and_place_graph(df, y_column, frame, color):
@@ -78,8 +78,11 @@ def create_and_place_graph(df, y_column, frame, color):
 
     Параметр color - цвет графика (строка, задаётся через HEX или названием цвета на англ.
     """
-    fig, ax = make_figure()
-    do_graph(df, ax, y_column, color)
+    frame_width = frame.winfo_width()
+    frame_height = frame.winfo_height()
+    font_size = get_font_size(frame_width, frame_height) * 1.3
+    fig, ax = make_figure(figsize=(frame_width / 100, frame_height / 100))
+    do_graph(df, ax, y_column, color, font_size)
     fig.tight_layout()
     canvas = FigureCanvasTkAgg(fig, master=frame)
     canvas.get_tk_widget().pack(fill="both", expand=1)
